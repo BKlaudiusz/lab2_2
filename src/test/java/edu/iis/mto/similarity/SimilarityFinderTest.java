547c999b1bdaec50f056504b1b2e4c967db9058d
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-03-22 19:09:29 +0100

    testy stanu - zmiana nazwy

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -22,47 +22,46 @@ class SimilarityFinderTest {
         }
     });
     @Test
-    void test1() {
+    void similarityFinderArrayIsTheSame() {
 
         int[] seq1 = {1,2,3,4};
         int[] seq2 = {1,2,3,4};
         assertEquals(1, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
     @Test
-    void test2() {
+    void  similarityFinderArrayIsDiffrend() {
 
         int[] seq1 = {1,2,3,4};
         int[] seq2 = {1,2,3,4,5,6};
         assertEquals(0.6666666666666666, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
     @Test
-    void test3() {
+    void similarityFinderArrayIsDiffrend2() {
 
         int[] seq1 = {2,4,5,6};
         int[] seq2 = {1,2,3,4,5,6,9};
         assertEquals(0.5714285714285714, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
     @Test
-    void test4() {
+    void similarityFinderArrayIsdifferent() {
 
         int[] seq1 = {4,5,6,7};
         int[] seq2 = {0,1,2,3};
         assertEquals(0, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
     @Test
-    void test5() {
+    void similarityFinderEmpty() {
 
         int[] seq1 = {};
         int[] seq2 = {};
         assertEquals(1, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
     @Test
-    void test6() {
+    void similarityFinderHalfArray() {
 
         int[] seq1 = {1,2};
         int[] seq2 = {1,2,3,4};
         assertEquals(0.5, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
-
 
 }
